02bf50d048ff570b4d6ab5eb0bd6724f7afbc951
ingui-n <inguin@email.cz>
2025-11-05 13:25:52 +0100

    hopefully fixed forwarding json bodies to microservices

--- routes/commonForwarder.js
+++ routes/commonForwarder.js
@@ -5,17 +5,27 @@ export const commonForwarder = async (req, forwardUrl, payload) => {
   url.host = redirectUrl.host;
   url.protocol = redirectUrl.protocol;
 
+  let body = null;
+
+  try {
+    const jsonBody = await req.body.json();
+    body = JSON.stringify(jsonBody);
+  } catch (e) {
+    return new Response("Bad request", {status: 400});
+  }
+
   return fetch(url, {
     method: req.method,
     headers: {
       ...req.headers,
+      'Content-Type': 'application/json',
       'X-User-Id': payload.sub,
       'X-User-Email': payload.email,
       'X-User-Firstname': payload.given_name,
       'X-User-Lastname': payload.family_name,
       'X-User-Roles': JSON.stringify(payload.realm_access.roles),
     },
-    body: req.body,
+    body: body,
     duplex: "half",
   })
     .catch((err) => {
